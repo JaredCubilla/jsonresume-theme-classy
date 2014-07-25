0c890aa91af1d04d78a4487e25420294e8653f0c
JaredCubilla <deraj.allibuc@gmail.com>
2014-07-25 18:42:03 -0400

    fixed bugs with null fields, attempted to add function to remove highlights

--- index.js
+++ index.js
@@ -56,24 +56,145 @@ function render(resumeObject) {
 	if (resumeObject.work) {
 		if (resumeObject.work[0].company) {
 			resumeObject.workBool = true;
+			_.each(resumeObject.work, function(w){
+				w.startDateYear = (w.startDate || "").substr(0,4);
+				if(w.endDate) {
+					w.endDateYear = (w.endDate || "").substr(0,4);
+				} else { 
+					w.endDateYear = 'Present'
+				}
+			});
+			_.each(resumeObject.work, function(w){
+				if (w.highlights) {
+					if (w.highlights[0] != "") {
+						w.workHighlights = true;
+					}
+				}
+			});
 		}
 	}
 
 	if (resumeObject.education) {
 		if (resumeObject.education[0].institution) {
 			resumeObject.educationBool = true;
+			_.each(resumeObject.education, function(e){
+			    if( !e.area || !e.studyType ){
+			      e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
+			    } else {
+			      e.educationDetail = e.area + ", "+ e.studyType;
+			    }
+				e.startDateYear = e.startDate.substr(0,4);
+				if (e.endDate) {
+					e.endDateYear = e.endDate.substr(0,4);
+					if (e.endDateYear > curyear) {
+						e.endDateYear += " (expected)";
+					}
+					if (e.endDateYear == curyear) {
+						e.endDateYear = 'Present';
+					}
+				} else { 
+					e.endDateYear = 'Present'
+				}
+			});
 		}
 	}
 
 	if (resumeObject.awards) {
 		if (resumeObject.awards[0].title) {
 			resumeObject.awardsBool = true;
+			_.each(resumeObject.awards, function(a){
+				a.year = (a.date || "").substr(0,4);
+				a.day = (a.date || "").substr(8,2);
+				switch ((a.date || "").substr(5,2)) {
+					case '01':
+						a.month = "January";
+						break;
+					case '02':
+						a.month = "February";
+						break;
+					case '03':
+						a.month = "March";
+						break;
+					case '04':
+						a.month = "April";
+						break;
+					case '05':
+						a.month = "May";
+						break;
+					case '06':
+						a.month = "June";
+						break;
+					case '07':
+						a.month = "July";
+						break;
+					case '08':
+						a.month = "August";
+						break;
+					case '09':
+						a.month = "September";
+						break;
+					case '10': 
+						a.month = "October";
+						break;
+					case '11':
+						a.month = "November";
+						break;
+					case '12':
+						a.month = "December";
+						break;
+				}
+			});
 		}
 	}
 
 	if (resumeObject.publications) {
 		if (resumeObject.publications[0].name) {
 			resumeObject.publicationsBool = true;
+			_.each(resumeObject.publications, function(a){
+				a.year = (a.releaseDate || "").substr(0,4);
+				a.day = (a.releaseDate || "").substr(8,2);
+				switch ((a.releaseDate || "").substr(5,2)) {
+					case '01':
+						a.month = "January";
+						break;
+					case '02':
+						a.month = "February";
+						break;
+					case '03':
+						a.month = "March";
+						break;
+					case '04':
+						a.month = "April";
+						break;
+					case '05':
+						a.month = "May";
+						break;
+					case '06':
+						a.month = "June";
+						break;
+					case '07':
+						a.month = "July";
+						break;
+					case '08':
+						a.month = "August";
+						break;
+					case '09':
+						a.month = "September";
+						break;
+					case '10': 
+						a.month = "October";
+						break;
+					case '11':
+						a.month = "November";
+						break;
+					case '12':
+						a.month = "December";
+						break;
+					default:
+						a.month = "Month";
+						break;
+				}
+			});
 		}
 	}
 
@@ -89,127 +210,9 @@ function render(resumeObject) {
 		}
 	}
 
+	
 
-	_.each(resumeObject.work, function(w){
-		w.startDateYear = (w.startDate || "").substr(0,4);
-		if(w.endDate) {
-			w.endDateYear = (w.endDate || "").substr(0,4);
-		} else { 
-			w.endDateYear = 'Present'
-		}
-	});
-
-	_.each(resumeObject.education, function(e){
-    if( !e.area || !e.studyType ){
-      e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
-    }  else {
-      e.educationDetail = e.area + ", "+ e.studyType;
-    }
-		e.startDateYear = e.startDate.substr(0,4);
-		if (e.endDate) {
-			e.endDateYear = e.endDate.substr(0,4);
-			if (e.endDateYear > curyear) {
-				e.endDateYear += " (expected)";
-			}
-			if (e.endDateYear == curyear) {
-				e.endDateYear = 'Present';
-			}
-		} else { 
-			e.endDateYear = 'Present'
-		}
-	});
-
-	_.each(resumeObject.awards, function(a){
-		a.year = a.date.substr(0,4);
-		a.day = a.date.substr(8,2);
-		switch (a.date.substr(5,2)) {
-			case '01':
-				a.month = "January";
-				break;
-			case '02':
-				a.month = "February";
-				break;
-			case '03':
-				a.month = "March";
-				break;
-			case '04':
-				a.month = "April";
-				break;
-			case '05':
-				a.month = "May";
-				break;
-			case '06':
-				a.month = "June";
-				break;
-			case '07':
-				a.month = "July";
-				break;
-			case '08':
-				a.month = "August";
-				break;
-			case '09':
-				a.month = "September";
-				break;
-			case '10': 
-				a.month = "October";
-				break;
-			case '11':
-				a.month = "November";
-				break;
-			case '12':
-				a.month = "December";
-				break;
-			default:
-				a.month = "Month";
-				break;
-		}
-	});
-
-	_.each(resumeObject.publications, function(a){
-		a.year = a.releaseDate.substr(0,4);
-		a.day = a.releaseDate.substr(8,2);
-		switch (a.releaseDate.substr(5,2)) {
-			case '01':
-				a.month = "January";
-				break;
-			case '02':
-				a.month = "February";
-				break;
-			case '03':
-				a.month = "March";
-				break;
-			case '04':
-				a.month = "April";
-				break;
-			case '05':
-				a.month = "May";
-				break;
-			case '06':
-				a.month = "June";
-				break;
-			case '07':
-				a.month = "July";
-				break;
-			case '08':
-				a.month = "August";
-				break;
-			case '09':
-				a.month = "September";
-				break;
-			case '10': 
-				a.month = "October";
-				break;
-			case '11':
-				a.month = "November";
-				break;
-			case '12':
-				a.month = "December";
-				break;
-			default:
-				a.month = "Month";
-				break;
-		}
-	});
+	
 	resumeObject.bio.capitalName = (resumeObject.bio.firstName + ' ' + resumeObject.bio.lastName).toUpperCase();
 
 	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
