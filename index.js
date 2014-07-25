476fccefce8d7ae48035b6a5abb2d73fa7eb4e63
JaredCubilla <deraj.allibuc@gmail.com>
2014-07-25 09:58:18 -0400

    added my own CSS, preparing for version 0.0.2

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var _ = require('lodash')
 var Mustache = require('mustache');
 var schema = require('resume-schema');
 
+var d = new Date();
+var curyear = d.getFullYear();
+
 var resumeObject = schema.resumeJson; 
 
 function render(resumeObject) {
@@ -17,6 +20,7 @@ function render(resumeObject) {
 			w.endDateYear = 'Present'
 		}
 	});
+
 	_.each(resumeObject.education, function(e){
     if( !e.area || !e.studyType ){
       e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
@@ -24,19 +28,112 @@ function render(resumeObject) {
       e.educationDetail = e.area + ", "+ e.studyType;
     }
 		e.startDateYear = e.startDate.substr(0,4);
-		if(e.endDate) {
+		if (e.endDate) {
 			e.endDateYear = e.endDate.substr(0,4);
-		}  else { 
+			if (e.endDateYear > curyear) {
+				e.endDateYear += " (expected)";
+			}
+			if (e.endDateYear == curyear) {
+				e.endDateYear = 'Present';
+			}
+		} else { 
 			e.endDateYear = 'Present'
 		}
 	});
-	if(resumeObject.bio && resumeObject.bio.email && resumeObject.bio.email.personal) {
-		resumeObject.bio.gravatar = gravatar.url(resumeObject.bio.email.personal, {
-                        s: '100',
-                        r: 'pg',
-                        d: 'mm'
-                    });
-	}
+
+	_.each(resumeObject.awards, function(a){
+		a.year = a.date.substr(0,4);
+		a.day = a.date.substr(8,2);
+		switch (a.date.substr(5,2)) {
+			case '01':
+				a.month = "January";
+				break;
+			case '02':
+				a.month = "February";
+				break;
+			case '03':
+				a.month = "March";
+				break;
+			case '04':
+				a.month = "April";
+				break;
+			case '05':
+				a.month = "May";
+				break;
+			case '06':
+				a.month = "June";
+				break;
+			case '07':
+				a.month = "July";
+				break;
+			case '08':
+				a.month = "August";
+				break;
+			case '09':
+				a.month = "September";
+				break;
+			case '10': 
+				a.month = "October";
+				break;
+			case '11':
+				a.month = "November";
+				break;
+			case '12':
+				a.month = "December";
+				break;
+			default:
+				a.month = "Month";
+				break;
+		}
+	});
+
+	_.each(resumeObject.publications, function(a){
+		a.year = a.releaseDate.substr(0,4);
+		a.day = a.releaseDate.substr(8,2);
+		switch (a.releaseDate.substr(5,2)) {
+			case '01':
+				a.month = "January";
+				break;
+			case '02':
+				a.month = "February";
+				break;
+			case '03':
+				a.month = "March";
+				break;
+			case '04':
+				a.month = "April";
+				break;
+			case '05':
+				a.month = "May";
+				break;
+			case '06':
+				a.month = "June";
+				break;
+			case '07':
+				a.month = "July";
+				break;
+			case '08':
+				a.month = "August";
+				break;
+			case '09':
+				a.month = "September";
+				break;
+			case '10': 
+				a.month = "October";
+				break;
+			case '11':
+				a.month = "November";
+				break;
+			case '12':
+				a.month = "December";
+				break;
+			default:
+				a.month = "Month";
+				break;
+		}
+	});
+
+	resumeObject.bio.capitalName = (resumeObject.bio.firstName + ' ' + resumeObject.bio.lastName).toUpperCase();
 
 	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
 	var resumeHTML = Mustache.render(theme, resumeObject);
