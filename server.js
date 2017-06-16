754bc1953c06d3f358fd95c745d79286efa15ebd
Reuben <reuben.coutinho@gmail.com>
2017-06-17 01:06:00 +1000

    Feature: load localhost

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var resume = require("resume-schema").resumeJson;
 var theme = require("./index.js");
+var fs = require('fs');
+const spawn = require('child_process').spawn;
 
 var port = 8080;
 http.createServer(function(req, res) {
@@ -10,10 +12,12 @@ http.createServer(function(req, res) {
 
 console.log("Serving theme");
 console.log("Preview: http://localhost:8080/");
+spawn('open', ['http://localhost:8080/']);
 
 function render() {
 	try {
-		return theme.render(resume);
+		var private_resume = JSON.parse(fs.readFileSync("../json-resume/resume.json", "utf-8"));
+		return theme.render(private_resume);
 	} catch(e) {
 		console.log("Error: " + e.message);
 		return "";
